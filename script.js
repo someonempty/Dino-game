8ae381530c0cacd4e58fb558e0b9b8bde55944a8
someonempty <someonempty@gmail.com>
2022-10-12 00:29:40 +0300

    commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const dino = document.getElementById('dino');
+const cactus = document.getElementById('cactus');
+
+document.addEventListener('keydown', (e) => {
+    jump();
+})
+
+const jump = () => {
+
+    if(dino.classList != 'jump') {
+        dino.classList.add('jump');
+    }
+
+    setTimeout(() => {
+        dino.classList.remove('jump')
+    }, 300)
+}
+
+let isAlive = setInterval(() => {
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
+
+    if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert('GAME OVER!');
+    }
+}, 10)
